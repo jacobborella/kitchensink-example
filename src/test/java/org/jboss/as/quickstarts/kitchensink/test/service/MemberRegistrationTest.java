aa0109fa1a03c9a1434dcfa88efb08cc27decb57
jacobborella <jborella@localhost.localdomain>
2017-03-25 22:38:49 +0100

    m

---
+++ src/test/java/org/jboss/as/quickstarts/kitchensink/test/service/MemberRegistrationTest.java
@@ -0,0 +1,45 @@
+package org.jboss.as.quickstarts.kitchensink.test.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.quickstarts.kitchensink.controller.MemberRegistration;
+import org.jboss.as.quickstarts.kitchensink.model.Member;
+import org.jboss.as.quickstarts.kitchensink.util.Resources;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.experimental.categories.Category;
+import org.jboss.shrinkwrap.impl.base.filter.ExcludeRegExpPaths;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.Mock;
+
+@RunWith(MockitoJUnitRunner.class)
+@Category(org.jboss.as.quickstarts.kitchensink.test.category.UnitTest.class)
+public class MemberRegistrationTest {
+   @Deployment
+   public static Archive<?> createTestArchive() {
+      return ShrinkWrap.create(WebArchive.class, "test.war")
+            .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+            // Deploy our test datasource
+            .addAsWebInfResource("test-ds.xml", "test-ds.xml");
+   }
+
+  @Mock
+  private MemberRegistration memberRegistration;
+
+   @Test
+   public void testRegister() throws Exception {
+      assertNotNull(new Object());
+   }
+   
+}
