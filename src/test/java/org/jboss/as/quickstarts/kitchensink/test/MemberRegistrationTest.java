24e4ebdc12ca5bccdacafe1bb2b8171e190bc818
jacobborella <jacobborella@hotmail.com>
2017-03-25 21:44:54 +0100

    Update MemberRegistrationTest.java

--- src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationTest.java
+++ src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationTest.java
@@ -8,21 +8,43 @@ import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.quickstarts.kitchensink.controller.MemberRegistration;
+import org.jboss.as.quickstarts.kitchensink.model.Member;
+import org.jboss.as.quickstarts.kitchensink.util.Resources;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.experimental.categories.Category;
-import org.jboss.as.quickstarts.kitchensink.test.category.UnitTest;
-import org.jboss.shrinkwrap.impl.base.filter.ExcludeRegExpPaths;
 
 @RunWith(Arquillian.class)
-@Category(UnitTest.class)
 public class MemberRegistrationTest {
-  @Test
-   public void testRegister() throws Exception {
-      assetNotNull(new Object());
+   @Deployment
+   public static Archive<?> createTestArchive() {
+      return ShrinkWrap.create(WebArchive.class, "test.war")
+            .addClasses(Member.class, MemberRegistration.class, Resources.class)
+            .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+            // Deploy our test datasource
+            .addAsWebInfResource("test-ds.xml", "test-ds.xml");
    }
+
+   @Inject
+   MemberRegistration memberRegistration;
+
+   @Inject
+   Logger log;
+
+   @Test
+   public void testRegister() throws Exception {
+      Member newMember = memberRegistration.getNewMember();
+      newMember.setName("Jane Doe");
+      newMember.setEmail("jane@mailinator.com");
+      newMember.setPhoneNumber("2125551234");
+      memberRegistration.register();
+      assertNotNull(newMember.getId());
+      log.info(newMember.getName() + " was persisted with id " + newMember.getId());
+   }
+   
 }
