29a2bb7063fa892984b5aa410846a98ad1d6c037
jacobborella <jborella@localhost.localdomain>
2017-03-25 19:18:34 +0100

    unit test added

--- src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationTest.java
+++ src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationTest.java
@@ -22,7 +22,17 @@ import org.junit.experimental.categories.Category;
 @Category(org.jboss.as.quickstarts.kitchensink.test.category.UnitTest.class)
 @RunWith(Arquillian.class)
 public class MemberRegistrationTest {
-  @Test
+   @Deployment
+   public static Archive<?> createTestArchive() {
+      return ShrinkWrap.create(WebArchive.class, "test.war")
+            .addClasses(Member.class, MemberRegistration.class, Resources.class)
+            .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+            // Deploy our test datasource
+            //             .addAsWebInfResource("test-ds.xml", "test-ds.xml");
+            //                }
+            //
+   @Test
    public void testRegister() throws Exception {
      assertNotNull(new Object());
    }
